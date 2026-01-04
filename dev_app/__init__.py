f7cc3426c3dd8604e98c9325804003ea4f99d45c
Brian Hooke <brian.hooke@mason.build>
2026-01-04 15:04:46 +1100

    Add version tracking - increment to v1.1.0
    - Add dev_app/version.py with version information
    - Update dev_app/__init__.py to expose version
    - Version 1.1.0: Recent activities and action items updates

---
+++ dev_app/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__, __version_info__
+
+# Make version available at package level
+__all__ = ['__version__', '__version_info__']
---
+++ dev_app/version.py
@@ -0,0 +1,6 @@
+"""
+Dev App Version Information
+"""
+
+__version__ = "1.1.0"
+__version_info__ = (1, 1, 0)
